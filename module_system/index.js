2dcad6760817b67a5b0524b27f107c5572245740
Medul Hasan <medul.cse@gmail.com>
2021-07-13 16:43:04 +0600

    module_system

---
+++ module_system/index.js
@@ -0,0 +1,6 @@
+// console.log(global);
+// console.log(__dirname);
+// console.log(__filename);
+
+var people = require('./people');
+console.log(people);
---
+++ module_system/people.js
@@ -0,0 +1,6 @@
+// (functio (exports, require, module, __filename, __dirname){      //module wrapper function
+var people = ["Montu", "Juntu", "Poltu"]
+
+module.exports = people
+// return module.exports
+// }) ();
